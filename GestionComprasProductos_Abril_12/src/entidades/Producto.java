63a5efbe04f0adeac83419dd850b77247d8e0bfd
JLGG <hotolete@gmail.com>
2016-04-25 01:59:02 +0200

    Reescritura toString clase Producto

--- GestionComprasProductos_Abril_12/src/entidades/Producto.java
+++ GestionComprasProductos_Abril_12/src/entidades/Producto.java
@@ -3,41 +3,48 @@ package entidades;
 public class Producto {
 	private int codigo;
 	private String nombre;
-	private double precio; 
-	
-	public Producto(){
-		this(0, "", 0); 
+	private double precio;
+
+	public Producto() {
+		this(0, "", 0);
 	}
-	
-	
+
 	public Producto(int codigo, String nombre, double precio) {
 		setCodigo(codigo);
 		setNombre(nombre);
 		setPrecio(precio);
 	}
 
+	public int getCodigo() {
+		return this.codigo;
+	}
 
-	public int getCodigo(){
-		return this.codigo; 
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
 	}
-	
-	public void setCodigo(int codigo){
-		this.codigo = codigo; 
+
+	public String getNombre() {
+		return nombre;
 	}
-	
-	public String getNombre(){
-		return nombre; 
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
-	
-	public void setNombre(String nombre){
-		this.nombre = nombre; 
+
+	public double getPrecio() {
+		return precio;
 	}
-	
-	public double getPrecio(){
-		return precio; 
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
 	}
-	
-	public void setPrecio(double precio){
-		this.precio = precio; 
+
+	@Override
+	public String toString() {
+		return "[codigo=" + this.getCodigo() 
+			+ ", nombre=" + this.getNombre() 
+			+ ", precio=" + this.getPrecio() 
+			+ "]";
 	}
+
 }
