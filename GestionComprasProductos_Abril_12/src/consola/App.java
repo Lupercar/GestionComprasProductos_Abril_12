3e99072066168858b318ffc0fa75ff039645068b
JLGG <hotolete@gmail.com>
2016-04-24 23:33:22 +0200

    Quinto - Métodos getter/setter de la clase Producto, le pongo datos para comprobar.

--- GestionComprasProductos_Abril_12/src/consola/App.java
+++ GestionComprasProductos_Abril_12/src/consola/App.java
@@ -13,5 +13,17 @@ public class App {
 		System.out.println(cadena);
 		
 		Producto p1 = new Producto(); 
+		p1.setCodigo(1);
+		p1.setNombre("Producto 1");
+		p1.setPrecio(2.00);
+		
+		Producto p2 = new Producto(); 
+		p2.setCodigo(2);
+		p2.setNombre("Producto 2");
+		p2.setPrecio(3.00);
+		
+		System.out.println("Nombre del producto 1: "+p1.getNombre());
+		System.out.println("Nombre del producto 2: "+p2.getNombre());
+		
 	}
 }
