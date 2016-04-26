6b1a3be1d8968faea26e89ce8105de1ce816e7cc
luperca <hotolete@gmail.com>
2016-04-26 09:14:05 +0200

    Añado método AddProducto y existeProducto en clase RepositorioCompras

--- GestionComprasProductos_Abril_12/src/consola/App.java
+++ GestionComprasProductos_Abril_12/src/consola/App.java
@@ -7,34 +7,52 @@ import java.util.Date;
 
 import entidades.Compra;
 import entidades.Producto;
+import entidades.RepositorioCompras;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String cadena = "Hola, mundo!!";
-		System.out.println(cadena);
-
-		// Producto p1 = new Producto();
-		// p1.setCodigo(1);
-		// p1.setNombre("Producto 1");
-		// p1.setPrecio(2.00);
-
-		Producto p1 = new Producto(1, "Producto 1", 2.00);
-
-		Producto p2 = new Producto();
-		p2.setCodigo(2);
-		p2.setNombre("Producto 2");
-		p2.setPrecio(3.00);
-
-		System.out.println("Nombre del producto 1: " + p1.getNombre());
-		System.out.println("Nombre del producto 2: " + p2.getNombre());
-
-		Compra c1 = new Compra(1, 2, new Date(), p1);
-		System.out.println("Compra - ID: " + c1.getCodigo());
-		System.out.println("Compra - CANTIDAD: " + c1.getCantidad());
-		System.out.println("Compra - FECHA: " + c1.getFecha());
-		System.out.println("Compra - PRODUCTO: " + c1.getProducto());
-		System.out.println("Compra - PRECIO COMPRA: " + c1.getPrecioCompra()+"€");
+//		String cadena = "Hola, mundo!!";
+//		System.out.println(cadena);
+//
+//		// Producto p1 = new Producto();
+//		// p1.setCodigo(1);
+//		// p1.setNombre("Producto 1");
+//		// p1.setPrecio(2.00);
+//
+//		Producto p1 = new Producto(1, "Producto 1", 2.00);
+//
+//		Producto p2 = new Producto();
+//		p2.setCodigo(2);
+//		p2.setNombre("Producto 2");
+//		p2.setPrecio(3.00);
+//
+//		System.out.println("Nombre del producto 1: " + p1.getNombre());
+//		System.out.println("Nombre del producto 2: " + p2.getNombre());
+//
+//		Compra c1 = new Compra(1, 2, new Date(), p1);
+//		System.out.println("Compra - ID: " + c1.getCodigo());
+//		System.out.println("Compra - CANTIDAD: " + c1.getCantidad());
+//		System.out.println("Compra - FECHA: " + c1.getFecha());
+//		System.out.println("Compra - PRODUCTO: " + c1.getProducto());
+//		System.out.println("Compra - PRECIO COMPRA: " + c1.getPrecioCompra()+"€");
+		
+		//comento todo y creo un repositorio de compras
+		
+		//creo primero 2 productos
+		Producto p1 = new Producto(1,"Producto1",1.00);
+		Producto p2 = new Producto(2,"Producto2",2.00);
+		
+		
+		RepositorioCompras repo = new RepositorioCompras(); 
+		
+		//Añado productos a RepositorioCompras
+		repo.addProducto(p1);
+		repo.addProducto(p2);
+		
+		//Añado un compra
+		repo.addCompra(p1, new Date(), 1);
+		repo.addCompra(p2, new Date(), 2);
 	}
 }
--- GestionComprasProductos_Abril_12/src/entidades/RepositorioCompras.java
+++ GestionComprasProductos_Abril_12/src/entidades/RepositorioCompras.java
@@ -5,17 +5,18 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * RepositorioCompras aplica el patrón JavaBeans
- * Propiedades privadas
- * Constructor sin parámetros
- * Getter-setter públicos
+ * RepositorioCompras aplica el patrón JavaBeans Propiedades privadas
+ * Constructor sin parámetros Getter-setter públicos
+ * 
  * @author JLGG
  *
  */
 public class RepositorioCompras {
-	private List<Producto> productos = new ArrayList<Producto>(); //con Java6 es obligatorio 
+	private List<Producto> productos = new ArrayList<Producto>(); // con Java6
+																	// es
+																	// obligatorio
 	private List<Compra> compras = new ArrayList<>();
-	
+
 	public RepositorioCompras() {
 	}
 
@@ -34,34 +35,65 @@ public class RepositorioCompras {
 	public void setCompras(List<Compra> compras) {
 		this.compras = compras;
 	}
-	
-	public void addCompra(Producto producto, Date fecha, int cantidad){
-		Compra compra = new Compra(nuevoCodigoCompra(), cantidad, fecha, producto); //nos falta el código
-		addCompra(compra); 
-	}
-	
-	private int nuevoCodigoCompra() {
-		int codigo = 0; 
-		if(compras.isEmpty()) //comprobamos si lista vacía, me lo puedo ahorrar
-			codigo++; 
-		else {
-			//1. Recorrer compras buscando el valor de código máximo
-			for (Compra p : compras) { //for ampliado o bucle for each
-				
-				if(p.getCodigo() > codigo) // Si el código del producto es superior 
-										   // al que tenemos, actualizamos el valor.
-					codigo = p.getCodigo(); 
-				
-			}
-			//2. Sumamos uno a ese valor de código máximo. 
-			codigo++; //equivalente a -> codigo = codigo + 1; 
-			
-		}
-		//3. Retornamos el valor del código
-		return codigo; 
+
+	public void addCompra(Producto producto, Date fecha, int cantidad) {
+		Compra compra = new Compra(nuevoCodigoCompra(), cantidad, fecha, producto); // nos
+																					// falta
+																					// el
+																					// código
+		addCompra(compra);
 	}
 
-	private void addCompra(Compra compra){
-		compras.add(compra); 
+	private int nuevoCodigoCompra() {
+		int codigo = 0;
+		if (compras.isEmpty()) // comprobamos si lista vacía, me lo puedo
+								// ahorrar
+			codigo++;
+		else {
+			// 1. Recorrer compras buscando el valor de código máximo
+			for (Compra p : compras) { // for ampliado o bucle for each
+
+				if (p.getCodigo() > codigo) // Si el código del producto es
+											// superior
+											// al que tenemos, actualizamos el
+											// valor.
+					codigo = p.getCodigo();
+
+			}
+			// 2. Sumamos uno a ese valor de código máximo.
+			codigo++; // equivalente a -> codigo = codigo + 1;
+
+		}
+		// 3. Retornamos el valor del código
+		return codigo;
+	}
+
+	private void addCompra(Compra compra) {
+		compras.add(compra);
+	}
+
+	public void addProducto(Producto producto) {
+		// comprobar que el producto sea válido (que apunte a algún sitio o sea
+		// != null)
+		if (producto != null) {
+
+			// miramos que no exista ya
+			if (!existeProducto(producto)) {
+				// Sino existe añado el Producto
+				productos.add(producto);
+			} else
+				System.err.println("Producto ya existe."); //err canal de error
+		} else
+			System.err.println("Producto no válido.");//err texto en color rojo
+	}
+
+	private boolean existeProducto(Producto producto) {
+		boolean retorno = false;
+
+		for (Producto p : productos) {
+			if (producto.getCodigo() == p.getCodigo())
+				retorno = true; // si son iguales existe por tanto es true
+		}
+		return retorno;
 	}
 }
